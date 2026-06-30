96d2c3c8c24aabc4195cc076ad8c196f278c8401
orangetbilisi <tornike.popkhadzee@gmail.com>
2026-06-30 10:34:36 +0400

    Add JSON parsing and webhook endpoint

--- server.js
+++ server.js
@@ -3,10 +3,18 @@ const path = require("path");
 
 const app = express();
 
+app.use(express.json());
 app.use(express.static(__dirname));
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
+});
+
+const bot = require("./bot");
+
+app.post("/webhook", (req, res) => {
+  bot.processUpdate(req.body);
+  res.sendStatus(200);
 });
 
 const PORT = process.env.PORT || 3000;

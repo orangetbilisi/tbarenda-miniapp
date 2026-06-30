a9bf63352b7efc2e76fc06de9f9d8c1972085b5f
orangetbilisi <tornike.popkhadzee@gmail.com>
2026-06-30 10:29:25 +0400

    Disable polling and remove channel post handling
    Disabled polling for the Telegram bot and removed the channel post handler that saves posts to a JSON file.

--- bot.js
+++ bot.js
@@ -2,34 +2,7 @@ const TelegramBot = require("node-telegram-bot-api");
 const fs = require("fs");
 
 const bot = new TelegramBot(process.env.BOT_TOKEN, {
-  polling: true,
-});
-
-bot.on("channel_post", (msg) => {
-  const text = msg.caption || msg.text || "";
-
-  let posts = [];
-
-  try {
-    posts = JSON.parse(fs.readFileSync("data.json", "utf8"));
-  } catch (e) {
-    posts = [];
-  }
-
-  const image = "https://picsum.photos/600/400";
-
-  posts.unshift({
-    title: text.split("\n")[0],
-    price: "",
-    description: text,
-    image: image,
-  });
-
-  posts = posts.slice(0, 50);
-
-  fs.writeFileSync("data.json", JSON.stringify(posts, null, 2));
-
-  console.log("NEW POST SAVED");
+  polling: false,
 });
 
 console.log("Bot started");
